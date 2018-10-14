60eb7696007c7cf046bfebde427f632cb4e976df
dinothebuddy <43946770+dinothebuddy@users.noreply.github.com>
2018-10-14 23:56:48 +0700

    Add files via upload

--- HelloBnS.java
+++ HelloBnS.java
@@ -1,12 +1,16 @@
 import java.util.*;
+import java.io.*;
 
 public class HelloBnS{
 	
-	public static void main(String[] args){		
+	public static void main(String[] args) throws IOException{		
 		System.out.println("Hello World This is TDTU");
-		System.out.print("Name: ");
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
-		System.out.println("Hello World I am " + name);
+		FileReader fr = new FileReader("name.txt");
+		Scanner sc = new Scanner(fr);
+		String name;
+		while(sc.hasNextLine()){
+			name = sc.nextLine();
+			System.out.println("Hello World I am " + name);
+		}
 	}
 }
