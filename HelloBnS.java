6431a81b5b0c8e44979ad37955e04792dccbafe1
dinothebuddy <43946770+dinothebuddy@users.noreply.github.com>
2018-10-14 23:44:36 +0700

    Add files via upload

--- HelloBnS.java
+++ HelloBnS.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class HelloBnS{
 	
 	public static void main(String[] args){
-		System.out.println("Hello World I am Hoang Viet Anh");
+		System.out.print("Name: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("Hello World I am " + name);
 	}
 }
